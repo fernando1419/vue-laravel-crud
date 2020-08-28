69a2553936711c2c159150c9c07550da15ffc7c3
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-08-28 15:14:06 -0300

    Refs #73148d3, using Route facade for routing

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-
-// use Illuminate\Support\Facades\Route;
 
 Route::get('/', function ()
 {
