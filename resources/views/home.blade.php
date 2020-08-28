8a9aa14171fd5251885de8c7eec98da808af6fbb
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-08-28 15:33:24 -0300

    Edited home view that will be handled with vue components later on

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">En que estás pensando ahora?</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,9 +14,31 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="thought">Ahora estoy pensando en:</label>
+                            <input type="text" name="thought" id="thought">
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-sm">Enviar Pensamiento</button>
+                    </form>
                 </div>
             </div>
+
+            <div class="card">
+                <div class="card-header">Publicado en 10/12/2020</div>
+
+                <div class="card-body">
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus aspernatur doloremque sit quas inventore?
+                    Tenetur cum corrupti voluptate dolore? Impedit esse reiciendis obcaecati autem! Quibusdam incidunt natus facere
+                    quaerat atque!</p>
+                </div>
+
+                <div class="card-footer">
+                    <button class="btn btn-light btn-sm">Editar</button>
+                    <button class="btn btn-danger btn-sm">Eliminar</button>
+                </div>
+            </div>
+
         </div>
     </div>
 </div>
